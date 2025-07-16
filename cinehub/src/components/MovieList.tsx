a78a72657fb082df84c5656a6e23bba128bbb127
feliperandon <feliperandon32@gmail.com>
2025-07-16 14:07:40 -0300

    getPopularMovies - api response render

--- cinehub/src/components/MovieList.tsx
+++ cinehub/src/components/MovieList.tsx
@@ -1,52 +1,28 @@
+import { useEffect, useState } from "react";
+
 import type { Movie } from "../types/movie";
+
+import { getPopularMovies } from "../services/tmdb";
+
 import MovieCard from "./MovieCard";
 
 const MovieList = () => {
-  const movie: Movie = {
-    id: 123,
-    title: "Superman",
-    poster_path: "/src/assets/superman.jpg",
-    release_date: "2024-10-22",
-    vote_average: 8,
-  };
+  const [movies, setMovies] = useState<Movie[]>([]);
 
-  const movies = [
-    {
-      id: 123,
-      title: "Superman",
-      poster_path: "/src/assets/superman.jpg",
-      release_date: "2024-10-22",
-      vote_average: 8,
-    },
-    {
-      id: 12,
-      title: "Dune",
-      poster_path: "/src/assets/Dune.jpeg",
-      release_date: "2025-10-22",
-      vote_average: 2,
-    },
-    {
-      id: 12333,
-      title: "Ghost Rider",
-      poster_path: "/src/assets/ghostrider.jpg",
-      release_date: "2007-10-22",
-      vote_average: 6,
-    },
-    {
-      id: 15,
-      title: "Bastardos Inglórios",
-      poster_path: "/src/assets/bastardos.jpg",
-      release_date: "2014-09-27",
-      vote_average: 10,
-    },
-  ];
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getPopularMovies();
+        setMovies(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
-    <div>
-      {movies.map((m) => (
-        <MovieCard movie={m} />
-      ))}
-    </div>
+    <div>{movies && movies.map((m) => <MovieCard key={m.id} movie={m} />)}</div>
   );
 };
 
--- cinehub/src/services/tmdb.ts
+++ cinehub/src/services/tmdb.ts
@@ -3,11 +3,15 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
+    api_key: import.meta.env.VITE_TMDB_API_KEY,
     language: "en-US",
   },
 });
 
-export const getMovieById = async (id: string) => {
-  const response = await api.get(`/movie/${id}`);
-  return response.data;
+export const getPopularMovies = async () => {
+  const {
+    data: { results },
+  } = await api.get("/movie/popular");
+  console.log(results);
+  return results;
 };
