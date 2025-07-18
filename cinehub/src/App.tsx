312e4e94478575a186c4253e142c3a1994324ebc
feliperandon <feliperandon32@gmail.com>
2025-07-18 14:12:10 -0300

    Hero.tsx (first design)

--- cinehub/src/App.tsx
+++ cinehub/src/App.tsx
@@ -1,10 +1,12 @@
+import Hero from "./components/Hero";
 import MovieList from "./components/MovieList";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className="bg-zinc-950">
+    <div className="bg-zinc-950 text-white min-h-screen">
       <Navbar />
+      <Hero />
       {/* <MovieList /> */}
     </div>
   );
--- cinehub/src/components/Hero.tsx
+++ cinehub/src/components/Hero.tsx
@@ -1,5 +1,13 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="flex justify-center">
+      <img
+        src="src/assets/banner.jpg"
+        alt="banner"
+        className="w-[1400px] rounded-2xl hover:opacity-80"
+      />
+    </div>
+  );
 };
 
 export default Hero;
