0c9e3a10eebdb89d8db7c6059741bc27cac32adc
feliperandon <feliperandon32@gmail.com>
2025-07-24 16:16:05 -0300

    navbar mobile responsive

--- cinehub/src/components/Navbar.tsx
+++ cinehub/src/components/Navbar.tsx
@@ -2,17 +2,17 @@ import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div className="text-white flex flex-col md:flex-row justify-between px-12 h-20 items-center my-6 md:my-0">
+    <div className="text-white flex justify-between px-12 h-20 items-center">
       <a href="#">
         <h1 className="uppercase font-bold text-3xl ">Cinehub</h1>
       </a>
       <div className="flex items-center gap-30">
-        <div className="flex gap-14 font-semibold">
+        <div className="gap-14 font-semibold hidden sm:flex">
           <a href="#">Home</a>
           <a href="#">Popular</a>
           <a href="#">About</a>
         </div>
-        <FaBars className="hidden md:block h-7 w-7 cursor-pointer " />
+        <FaBars className="h-7 w-7 cursor-pointer sm:hidden" />
       </div>
     </div>
   );
