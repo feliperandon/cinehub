5bf8026d938abbee113c47252dee4c856e03539a
feliperandon <feliperandon32@gmail.com>
2025-09-10 14:41:12 -0300

    implement modal close on esc and outside click

--- cinehub/src/components/MovieModal.tsx
+++ cinehub/src/components/MovieModal.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import type { Movie } from "../types/movie";
 
 type MovieModalProps = {
@@ -6,9 +7,28 @@ type MovieModalProps = {
 };
 
 export function MovieModal({ movie, onClose }: MovieModalProps) {
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [onClose]);
+
   return (
-    <div className="fixed inset-0 bg-black/70 flex items-center justify-center z-50">
-      <div className="bg-zinc-900 text-white p-6 rounded-lg max-w-lg w-full relative">
+    <div
+      className="fixed inset-0 bg-black/70 flex items-center justify-center z-50"
+      onClick={onClose}
+    >
+      <div
+        className="bg-zinc-900 text-white p-6 rounded-lg max-w-lg w-full relative"
+        onClick={(e) => e.stopPropagation()}
+      >
         <button
           onClick={onClose}
           className="absolute top-2 right-2 text-gray-400 hover:text-white cursor-pointer"
